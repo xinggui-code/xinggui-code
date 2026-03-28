755974f8abcf874204784c6442a6603f026ec884
xinggui-code <1814176478@qq.com>
2026-03-28 16:29:27 +0800

    Create maxkb-float.js

---
+++ maxkb-float.js
@@ -0,0 +1,7 @@
+document.write(`
+<iframe
+src="https://yourname.github.io/maxkb-float.html"
+style="position:fixed; right:20px; bottom:20px; width:0; height:0; border:none;"
+onload="this.style.width='auto'; this.style.height='auto'; this.style.zIndex='9999';">
+</iframe>
+`);
